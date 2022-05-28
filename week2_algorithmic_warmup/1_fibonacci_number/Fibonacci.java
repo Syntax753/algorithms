a7715bcca3b0daf87db296b825fdb217de419edf
Peter Turner <syntax.valid@gmail.com>
2022-05-28 19:36:01 +0100

    Fibonacci tick

--- week2_algorithmic_warmup/1_fibonacci_number/Fibonacci.java
+++ week2_algorithmic_warmup/1_fibonacci_number/Fibonacci.java
@@ -1,17 +1,27 @@
 import java.util.Scanner;
 
 public class Fibonacci {
-  private static long calc_fib(int n) {
-    if (n <= 1)
-      return n;
+    private static long calc_fib(int n) {
+        int[] fibList = new int[Math.max(2, n + 1)];
+        fibList[0] = 0;
+        fibList[1] = 1;
 
-    return calc_fib(n - 1) + calc_fib(n - 2);
-  }
+        for (int i = 2; i < fibList.length; i++) {
+            fibList[i] = fibList[i - 2] + fibList[i - 1];
+        }
 
-  public static void main(String args[]) {
-    Scanner in = new Scanner(System.in);
-    int n = in.nextInt();
+        // for (int i = 0; i < fibList.length; i++) {
+        //     System.out.println(fibList[i]);
+        // }
 
-    System.out.println(calc_fib(n));
-  }
+        return fibList[n];
+    }
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        System.out.println(calc_fib(n));
+        in.close();
+    }
 }
