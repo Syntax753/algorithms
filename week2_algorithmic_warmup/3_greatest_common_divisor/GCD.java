8b455e499893b4f7b0e549be13d64b3dd559c219
Peter Turner <syntax.valid@gmail.com>
2022-05-28 22:02:30 +0100

    Clean unused

--- week2_algorithmic_warmup/3_greatest_common_divisor/GCD.java
+++ week2_algorithmic_warmup/3_greatest_common_divisor/GCD.java
@@ -1,19 +1,6 @@
 import java.util.*;
 
 public class GCD {
-  private static int gcd_naive(int a, int b) {
-    int current_gcd = 1;
-    for (int d = 2; d <= a && d <= b; ++d) {
-      if (a % d == 0 && b % d == 0) {
-        if (d > current_gcd) {
-          current_gcd = d;
-        }
-      }
-    }
-
-    return current_gcd;
-  }
-
   private static int gcd_fast(int a, int b) {
     if (b == 0) {
       return a;
