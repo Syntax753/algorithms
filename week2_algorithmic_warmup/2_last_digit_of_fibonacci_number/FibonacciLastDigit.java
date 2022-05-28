ab2086107bd60c9ce9f9cf2254b5e244811efa23
Peter Turner <syntax.valid@gmail.com>
2022-05-28 21:58:50 +0100

    Fibonnaici Last Digit tick

--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/FibonacciLastDigit.java
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/FibonacciLastDigit.java
@@ -11,10 +11,10 @@ public class FibonacciLastDigit {
         for (int i = 0; i < n - 1; ++i) {
             int tmp_previous = previous;
             previous = current;
-            current = tmp_previous + current;
+            current = (tmp_previous + current)%10;
         }
 
-        return current % 10;
+        return current;
     }
     
     public static void main(String[] args) {
@@ -22,6 +22,7 @@ public class FibonacciLastDigit {
         int n = scanner.nextInt();
         int c = getFibonacciLastDigitNaive(n);
         System.out.println(c);
+        scanner.close();
     }
 }
 
