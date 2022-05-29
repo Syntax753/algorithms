cfcc6ad588586b65be90179a63f19eadc1a7c12e
Peter Turner <syntax.valid@gmail.com>
2022-05-29 18:01:18 +0100

    LCM tick

--- week2_algorithmic_warmup/4_least_common_multiple/LCM.java
+++ week2_algorithmic_warmup/4_least_common_multiple/LCM.java
@@ -9,11 +9,29 @@ public class LCM {
     return (long) a * b;
   }
 
+  private static long gcd_fast(int a, int b) {
+    if (b == 0) {
+      return a;
+    }
+
+    return gcd_fast(b, a % b);
+  }
+
+
+  private static long lcm_fast(int a, int b) {
+    if (a == 0  && b == 0) {
+      return 0;
+    }
+
+    return (long) a*b/gcd_fast(a, b);
+  }
+
   public static void main(String args[]) {
     Scanner scanner = new Scanner(System.in);
     int a = scanner.nextInt();
     int b = scanner.nextInt();
 
-    System.out.println(lcm_naive(a, b));
+    System.out.println(lcm_fast(a, b));
+    scanner.close();
   }
 }
