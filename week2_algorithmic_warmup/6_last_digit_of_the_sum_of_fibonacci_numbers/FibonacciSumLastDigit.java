9095edf35fcba04d46523d93c55888d0a7a14690
Peter Turner <syntax.valid@gmail.com>
2022-05-30 17:37:34 +0100

    Fibonacci Sums Last Digit tick

--- week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/FibonacciSumLastDigit.java
+++ week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/FibonacciSumLastDigit.java
@@ -1,29 +1,43 @@
 import java.util.*;
 
 public class FibonacciSumLastDigit {
-    private static long getFibonacciSumNaive(long n) {
+    private static long getFibonacciSumFast(long n) {
         if (n <= 1)
             return n;
 
         long previous = 0;
         long current  = 1;
-        long sum      = 1;
 
-        for (long i = 0; i < n - 1; ++i) {
+        List<Long> sums = new ArrayList<>();
+        sums.add(0L);
+        sums.add(1L);
+        Long sum = 1L;
+
+        for (long i = 0; i < n-1; ++i) {
             long tmp_previous = previous;
             previous = current;
-            current = tmp_previous + current;
-            sum += current;
+            current = (tmp_previous + current)%10;
+            sum+=current;
+            sum %=10;
+            sums.add(sum);
+
+            if (previous == 0 && current == 1) {
+                n = n%(i+1);
+                break;
+            }
+            
         }
 
-        return sum % 10;
+        // System.out.println(n + " "+ sums);
+        return sums.get((int) (n));
     }
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
-        long s = getFibonacciSumNaive(n);
+        long s = getFibonacciSumFast(n);
         System.out.println(s);
+        scanner.close();
     }
 }
 
