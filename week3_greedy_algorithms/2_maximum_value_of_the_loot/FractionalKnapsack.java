4cb14765eb58242e6d468f9edf1fabc977138ce2
Peter Turner <syntax.valid@gmail.com>
2022-05-30 19:28:00 +0100

    Fractional Knapsack tick

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/FractionalKnapsack.java
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/FractionalKnapsack.java
@@ -3,7 +3,27 @@ import java.util.Scanner;
 public class FractionalKnapsack {
     private static double getOptimalValue(int capacity, int[] values, int[] weights) {
         double value = 0;
-        //write your code here
+        // write your code here
+        while (capacity > 0) {
+            double max = 0;
+            int idx = -1;
+
+            for (int i = 0; i < values.length; i++) {
+                if (values[i] != 0 && values[i] / (double)weights[i] > max) {
+                    max = values[i] / (double) weights[i];
+                    idx = i;
+                }
+            }
+
+            if (idx == -1) {
+                break;
+            }
+
+            int w = weights[idx];
+            value += Math.min(w, capacity) * max;
+            capacity -= w;
+            values[idx] = 0;
+        }
 
         return value;
     }
@@ -19,5 +39,6 @@ public class FractionalKnapsack {
             weights[i] = scanner.nextInt();
         }
         System.out.println(getOptimalValue(capacity, values, weights));
+        scanner.close();
     }
-} 
+}
