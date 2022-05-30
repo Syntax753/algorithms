5b6519366f9e12a5ff413763e5c1009fa1e56e33
Peter Turner <syntax.valid@gmail.com>
2022-05-30 18:00:29 +0100

    Fibonacci Partial Sum tick

--- week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/FibonacciPartialSum.java
+++ week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/FibonacciPartialSum.java
@@ -1,30 +1,64 @@
 import java.util.*;
 
 public class FibonacciPartialSum {
-    private static long getFibonacciPartialSumNaive(long from, long to) {
-        long sum = 0;
+    private static long getFibonacciSumFast(long n) {
+        if (n <= 1)
+            return n;
 
-        long current = 0;
-        long next  = 1;
+        long previous = 0;
+        long current  = 1;
 
-        for (long i = 0; i <= to; ++i) {
-            if (i >= from) {
-                sum += current;
+        List<Long> sums = new ArrayList<>();
+        sums.add(0L);
+        sums.add(1L);
+        Long sum = 1L;
+
+        for (long i = 0; i < n-1; ++i) {
+            long tmp_previous = previous;
+            previous = current;
+            current = (tmp_previous + current)%10;
+            sum+=current;
+            sum %=10;
+            sums.add(sum);
+
+            if (previous == 0 && current == 1) {
+                n = n%(i+1);
+                break;
             }
-
-            long new_current = next;
-            next = next + current;
-            current = new_current;
+            
         }
 
-        return sum % 10;
+        // System.out.println(n + " "+ sums);
+        return sums.get((int) (n));
+    }
+
+    private static long getFibonacciPartialSumFast(long from, long to) {
+        if (to <= 1)
+            return to;
+
+        long fromSum = 0;
+        if (from > 1) {
+            fromSum = getFibonacciSumFast(from-1);
+        }  
+        long toSum = getFibonacciSumFast(to);
+
+        // System.out.println(fromSum);
+        // System.out.println(toSum);
+
+        Long sum = toSum-fromSum;
+
+        if (sum < 0) {
+            sum+= 10;
+        }
+        return sum;
     }
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long from = scanner.nextLong();
         long to = scanner.nextLong();
-        System.out.println(getFibonacciPartialSumNaive(from, to));
+        System.out.println(getFibonacciPartialSumFast(from, to));
+        scanner.close();
     }
 }
 
