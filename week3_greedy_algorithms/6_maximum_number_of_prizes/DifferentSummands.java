c3797247d80060717d104eaff5b850d6bfff208d
Peter Turner <syntax.valid@gmail.com>
2022-05-30 22:48:03 +0100

    Maximum Prizes tick

--- week3_greedy_algorithms/6_maximum_number_of_prizes/DifferentSummands.java
+++ week3_greedy_algorithms/6_maximum_number_of_prizes/DifferentSummands.java
@@ -3,10 +3,30 @@ import java.util.*;
 public class DifferentSummands {
     private static List<Integer> optimalSummands(int n) {
         List<Integer> summands = new ArrayList<Integer>();
-        //write your code here
+        int sqrt = (int) Math.sqrt((double) n);
+        // System.out.println(sqrt);
+
+        int top = 0;
+        int sum = 0;
+        for (int i = sqrt; sum <= n; i++) {
+            top = i - 1;
+            sum = i * (i + 1) / 2;
+        }
+        top--;
+
+        for (int i = 0; i < top; i++) {
+            summands.add(i + 1);
+        }
+
+        int addOn = n - top * (top + 1) / 2;
+
+        if (addOn > 0) {
+            summands.add(addOn);
+        }
+
         return summands;
     }
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
@@ -15,6 +35,6 @@ public class DifferentSummands {
         for (Integer summand : summands) {
             System.out.print(summand + " ");
         }
+        scanner.close();
     }
 }
-
