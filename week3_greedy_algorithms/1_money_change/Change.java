ce766e71bc2377e61ee57478eb3d7bd770b31963
Peter Turner <syntax.valid@gmail.com>
2022-05-30 18:46:33 +0100

    Money Change tick

--- week3_greedy_algorithms/1_money_change/Change.java
+++ week3_greedy_algorithms/1_money_change/Change.java
@@ -2,15 +2,21 @@ import java.util.Scanner;
 
 public class Change {
     private static int getChange(int m) {
-        //write your code here
-        return m;
+        int c = m / 10;
+        m %= 10;
+
+        if (m >= 5) {
+            c++;
+            m -= 5;
+        }
+
+        return c + m;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int m = scanner.nextInt();
         System.out.println(getChange(m));
-
+        scanner.close();
     }
 }
-
