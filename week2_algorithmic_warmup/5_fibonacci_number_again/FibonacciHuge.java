1cd559a1205d0d05370032a6915275c26964d3df
Peter Turner <syntax.valid@gmail.com>
2022-05-30 17:13:34 +0100

    Huge Fibonacci tick

--- week2_algorithmic_warmup/5_fibonacci_number_again/FibonacciHuge.java
+++ week2_algorithmic_warmup/5_fibonacci_number_again/FibonacciHuge.java
@@ -1,27 +1,41 @@
 import java.util.*;
 
 public class FibonacciHuge {
-    private static long getFibonacciHugeNaive(long n, long m) {
+    private static long getFibonacciHugeFast(long n, long m) {
         if (n <= 1)
             return n;
 
         long previous = 0;
-        long current  = 1;
+        long current = 1;
 
+        List<Long> mods = new ArrayList<>();
+        long cycle = 0;
         for (long i = 0; i < n - 1; ++i) {
+            mods.add(previous);
             long tmp_previous = previous;
             previous = current;
-            current = tmp_previous + current;
+            current = (tmp_previous + current)%m;
+
+            if (previous == 0 && current == 1) {
+                cycle = i + 1;
+                break;
+            }
         }
 
-        return current % m;
+        // System.out.println(mods+ " " +current);
+
+        if (cycle == 0) {
+            return current;
+        } else {
+            return mods.get((int) (n % cycle)) % m;
+        }
     }
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
         long m = scanner.nextLong();
-        System.out.println(getFibonacciHugeNaive(n, m));
+        System.out.println(getFibonacciHugeFast(n, m));
+        scanner.close();
     }
 }
-
