1ccdae5a862c16f2063ef6f56b97e8875596287a
Peter Turner <syntax.valid@gmail.com>
2022-06-06 14:30:14 +0100

    Largest Number tick

--- week3_greedy_algorithms/7_maximum_salary/LargestNumber.java
+++ week3_greedy_algorithms/7_maximum_salary/LargestNumber.java
@@ -3,16 +3,17 @@ import java.util.stream.Collectors;
 
 public class LargestNumber {
     private static String largestNumber(String[] a) {
-        //write your code here
 
         Arrays.sort(a, new Comparator<String>() {
 
             @Override
             public int compare(String o1, String o2) {
-                double max1 = Double.parseDouble("9".repeat(o1.length()));
-                double max2 = Double.parseDouble("9".repeat(o2.length()));
+                int a = Integer.parseInt(o1);
+                double aMax = Integer.parseInt(o1.replaceAll("[0-9]", "9"));
+                int b = Integer.parseInt(o2);
+                double bMax = Integer.parseInt(o2.replaceAll("[0-9]", "9"));
 
-                if (max1/Integer.parseInt(o1) > max2/Integer.parseInt(o2))
+                if (aMax/a > bMax/b)
                     return 1;
                 else
                 return -1;
